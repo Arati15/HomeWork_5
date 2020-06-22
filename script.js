80ffde844004dd5cf992b8f90dedc8048b740265
Arati Bhandwalkar <arativbhandwalkar@gmail.com>
2020-06-21 23:50:02 -0700

    changed comments

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
-let currentTime = new Date();
-let currentHour = currentTime.getHours();
-currentHour.toLocaleString()
+var currentTime = new Date();//date
+var currentHour = currentTime.getHours();//number
+currentHour.toLocaleString()//number
 
-let leadEl = $("#currentDay");
-leadEl.text(currentTime);
+var leadEl = $("#currentDay");//any
+leadEl.text(currentTime);//date
 
 
 document.addEventListener("click", function(event){
     event.preventDefault();
     if(event.target.type === 'submit'){
-         let index = event.target.getAttribute('data-time-index');
-    let inputfield = document.querySelector(`#content-${index}`).value;
+    var index = event.target.getAttribute('data-time-index');
+    var inputfield = document.querySelector(`#content-${index}`).value;
     console.log(index, inputfield);
-    localStorage.setItem(index, inputfield);
+    //set index and inputField to local storage
+    localStorage.setItem(index, inputfield);//new key value pair
     }
 })
 
-for (let i = 0; i < 9; i++){
-    let textArea = document.querySelector(`#content-${i}`);
-    let todo = localStorage.getItem(i) || ''; 
-    textArea.textContent = todo;
+//for loop interates 9 time that is 9 hour
+for (var i = 0; i < 9; i++){
+    //content 0....to content 8
+    var textArea = document.querySelector(`#content-${i}`);
+    //get item on localstorage as a text
+    var st = localStorage.getItem(i) || ''; 
+    textArea.textContent = st;//return
+    //if current hour is greater than i + 9 that mens it's past time
     if(i + 9 < currentHour){
         textArea.classList.add('past')
-    }else if(i + 9 === currentHour){
+    }
+    //else if is equle to current hour that means present
+    else if(i + 9 === currentHour){
         textArea.classList.add('present')
-    }else{
+    }
+    // if not that mean future time
+    else{
         textArea.classList.add('future')
     }
 }
